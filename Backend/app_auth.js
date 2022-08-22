cb3d3cd49cf77a5cf083070e2eeb2ee4b2d9b2da
Ignjat Krdzavac <ikrdzavac11520rn@raf.rs>
2022-08-22 02:15:06 +0200

    commit

--- Backend/app_auth.js
+++ Backend/app_auth.js
@@ -8,8 +8,10 @@ require('dotenv').config();
 const app = express();
 
 var corsOptions = {
-    origin: '*',
-    optionsSuccessStatus: 200
+    "origin": "*",
+    "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
+    "preflightContinue": false,
+    "optionsSuccessStatus": 204
 }
 
 app.use(express.json());
